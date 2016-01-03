55626d058b3abee8ebc8ad1b1850d03400471ebc
anthony <cfsn1@hotmail.com>
2016-01-02 22:31:58 -0500

    switch to rendered jade views

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // take arguments at startup
-if (process.argv[2] == "")  throw "Error: no ip specified in argument 1";
-if (process.argv[3] == "")  throw "Error: no port specified in argument 2";
+// the first is ip, second is port
+if (!process.argv[2])  throw "Error: no ip specified in argument 1";
+if (!process.argv[3])  throw "Error: no port specified in argument 2";
 
 var http_ip = process.argv[2];
 var http_port = process.argv[3];
@@ -8,30 +9,32 @@ var http_port = process.argv[3];
 var express = require('express');
 var app = express();
 
-//Uses
-app.use(express.static('public'));
-
-
+// using jade for out views
+app.set('view engine', 'jade');
 
 // Routes
+
+app.get('/css/*', function(req, res){
+	res.sendFile(__dirname + '/public'+ req.path);
+});
+
 app.get('/', function (req, res) {
+	res.render('../public/index', {'user_ip':req.connection.remoteAddress});
+
 });
 
 app.get('/projects', function (req, res) {
-	res.sendFile('./public/projects.html', { root : __dirname});
+	res.render('../public/projects');
 });
 
 //The 404 Route
 app.get('*', function(req, res){
 	res.send('Error: 404, page not found.', 404);
-});
-
-// end Routes
+});// end Routes
 
 //start server
 var server = app.listen(http_port, function () {
 	var host = http_ip;
 	var port = http_port;
-
 	console.log('amccann_io at http://%s:%s', host, port);
 });
